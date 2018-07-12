b982186c1d54b0aadba51fc4890b1452022d75d8
Andrew Jarombek <ajarombek95@gmail.com>
2018-07-11 21:35:13 -0400

    Set Up Basic Comparison Table

---
+++ src/client/ComparisonTable.js
@@ -0,0 +1,35 @@
+/**
+ * ComparisonTable Component
+ * @author Andrew Jarombek
+ * @since 7/11/2018
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ComparisonTable = ({ title, children, primaryColor, secondaryColor }) =>
+    <div className="jarombek-comparison-table">
+        <div className={`jarombek-ct-header-color-${primaryColor}`}>
+            <p className="jarombek-ct-title">{title}</p>
+            <div className={`jarombek-ct-body
+                jarombek-ct-primary-color-${primaryColor}
+                jarombek-ct-secondary-color-${secondaryColor}`}>
+
+                {children}
+            </div>
+        </div>
+    </div>;
+
+ComparisonTable.propTypes = {
+    children: PropTypes.any,
+    title: PropTypes.string.isRequired,
+    primaryColor: PropTypes.string,
+    secondaryColor: PropTypes.string
+};
+
+ComparisonTable.defaultProps = {
+    primaryColor: "default-primary",
+    secondaryColor: "default-secondary"
+};
+
+export default ComparisonTable;
