909b54a186a3928a8a9d2d4c8f5dbb6981ddb970
Andrew Jarombek <ajarombek95@gmail.com>
2018-09-13 19:15:08 -0400

    Basic Animations

--- src/client/Resume.js
+++ src/client/Resume.js
@@ -31,6 +31,7 @@ class Resume extends React.Component {
         this.state = {
             points,
             position,
+            prevPosition: position - 1,
             title,
             content,
             languages,
@@ -73,12 +74,14 @@ class Resume extends React.Component {
      */
     initResumeContent(props) {
         const {page} = queryString.parse(props.location.search);
-        const resumePage = +page || +props.position || 1;
+        const position = +page || +props.position || 1;
+        const prevPosition = this.state.position || position - 1;
 
-        const {title, content, languages, technologies} = resumeSections[resumePage - 1];
+        const {title, content, languages, technologies} = resumeSections[position - 1];
 
         this.setState({
-            position: resumePage,
+            position,
+            prevPosition,
             title,
             content,
             languages,
@@ -90,7 +93,16 @@ class Resume extends React.Component {
      * Render the JSX
      */
     render() {
-        const {subscribing, points, position, title, languages, technologies} = this.state;
+        const {
+            subscribing,
+            points,
+            position,
+            prevPosition,
+            title,
+            languages,
+            technologies
+        } = this.state;
+
         console.debug('Inside Resume Render');
 
         return (
@@ -107,7 +119,16 @@ class Resume extends React.Component {
                         <Timeline points={+points} position={+position} />
                     </div>
                     <div className="jarbek-resume-title">
-                        <h5>{ title }</h5>
+                        { resumeSections.map((item, index) =>
+                            <div key={`jarbek-resume-title-${index}`}
+                                 className={`${position === index + 1 ?
+                                     prevPosition < position ?
+                                         'jarbek-resume-title-active-right':
+                                         'jarbek-resume-title-active-left':
+                                     'jarbek-resume-title-inactive'}`}>
+                                <h5>{ item.title }</h5>
+                            </div>
+                        )}
                     </div>
                     { +position !== 1 ?
                         <Link className="jarbek-resume-prev" to={`/resume?page=${position - 1}`}>
@@ -116,9 +137,11 @@ class Resume extends React.Component {
                     }
                     <div className="jarbek-resume-content">
                         { resumeSections.map((item, index) =>
-                            <div key={`jarbek-resume-content-${index+1}`}
-                                 className={`${position === index+1 ?
-                                     'jarbek-resume-content-active':
+                            <div key={`jarbek-resume-content-${index + 1}`}
+                                 className={`${position === index + 1 ?
+                                     prevPosition < position ?
+                                         'jarbek-resume-content-active-right':
+                                         'jarbek-resume-content-active-left':
                                      'jarbek-resume-content-inactive'}`}>
                                 { item.content }
                             </div>
@@ -130,22 +153,31 @@ class Resume extends React.Component {
                         </Link>: null
                     }
                     <div className="jarbek-resume-tech">
-                        { languages.length ?
-                            <p className="jarbek-resume-tech-languages">
-                                <strong>Languages: </strong>
-                                {languages.reduce((acc, item, index) =>
-                                    index ? `${acc}, ${item}`: `${item}`
-                                )}
-                            </p>: null
-                        }
-                        {technologies.length ?
-                            <p className="jarbek-resume-tech-technologies">
-                                <strong>Technologies: </strong>
-                                {technologies.reduce((acc, item, index) =>
-                                    index ? `${acc}, ${item}` : `${item}`
-                                )}
-                            </p>: null
-                        }
+                        { resumeSections.map((item, index) =>
+                            <div key={`jarbek-resume-tech-${index + 1}`}
+                                 className={`${position === index + 1 ?
+                                     prevPosition < position ?
+                                         'jarbek-resume-tech-active-right':
+                                         'jarbek-resume-tech-active-left':
+                                     'jarbek-resume-tech-inactive'}`}>
+                                { item.languages.length ?
+                                    <p className="jarbek-resume-tech-languages">
+                                        <strong>Languages: </strong>
+                                        {item.languages.reduce((acc, item, index) =>
+                                            index ? `${acc}, ${item}`: `${item}`
+                                        )}
+                                    </p>: null
+                                }
+                                { item.technologies.length ?
+                                    <p className="jarbek-resume-tech-technologies">
+                                        <strong>Technologies: </strong>
+                                        {item.technologies.reduce((acc, item, index) =>
+                                            index ? `${acc}, ${item}` : `${item}`
+                                        )}
+                                    </p>: null
+                                }
+                            </div>
+                        )}
                     </div>
                 </div>
                 { (subscribing) ?
